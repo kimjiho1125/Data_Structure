b9de06908bb35696b0a5ecbce92d86b8cb763f63
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2022-01-16 18:35:55 +0900

    phoneBook02

--- phonebook01.c
+++ phonebook01.c
@@ -11,7 +11,9 @@ int n = 0;               /* number of people in phone directory*/
 void add();
 void find();
 void status();
-void remove1();
+void remove();
+void load();
+void save();
 
 int main()
 {
@@ -21,7 +23,11 @@ int main()
         printf("$ ");
         scanf("%s", command);
 
-        if (strcmp(command, "add") == 0)
+        if (strcmp(command, "read") == 0)
+        {
+            load();
+        }
+        else if (strcmp(command, "add") == 0)
         {
             add();
         }
@@ -35,7 +41,11 @@ int main()
         }
         else if (strcmp(command, "delete") == 0)
         {
-            remove1();
+            remove();
+        }
+        else if (strcmp(command, "save") == 0)
+        {
+            save();
         }
         else if (strcmp(command, "exit") == 0)
         {
@@ -51,47 +61,97 @@ void add()
     scanf("%s", buf1);
     scanf("%s", buf2);
 
-    names[n] = strdup(buf1); 
+    names[n] = strdup(buf1);
     numbers[n] = strdup(buf2);
 
     printf("$s was added successfully.\n", buf1);
 }
-void find(){
+void find()
+{
     char buf[BUFFER_SIZE];
-    scanf("%s",buf);
+    scanf("%s", buf);
 
     int i;
-    for(i=0;i<n;i++){
+    for (i = 0; i < n; i++)
+    {
         if (strcmp(buf, names[i]) == 0)
         {
             printf("%s\n", numbers[i]);
             return;
         }
     }
-    printf("No person named '%s' exist.\n",buf);
+    printf("No person named '%s' exist.\n", buf);
 }
 
-void status(){
+void status()
+{
     int i;
-    for(i=0;i<n;i++){
-        printf("%s %s\n", names[i],numbers[i]);
+    for (i = 0; i < n; i++)
+    {
+        printf("%s %s\n", names[i], numbers[i]);
     }
-    printf("Total %d persons.\n",n);
+    printf("Total %d persons.\n", n);
 }
 
-void remove1(){
+void remove()
+{
     char buf[BUFFER_SIZE];
     scanf("%s", buf);
 
     int i;
-    for(i=0;i<n;i++){
-        if(strcmp(buf,names[i])==0){
-            names[i] = names[n-1];
-            numbers[i] = numbers[n-1];
+    for (i = 0; i < n; i++)
+    {
+        if (strcmp(buf, names[i]) == 0)
+        {
+            names[i] = names[n - 1];
+            numbers[i] = numbers[n - 1];
             n--;
             printf("'%s' was deleted successfully. \n", buf);
             return;
         }
     }
-    printf("No person named '%s' exist. \n",buf);
+    printf("No person named '%s' exist. \n", buf);
+}
+void load()
+{
+    char fileName[BUFFER_SIZE];
+    char buf1[BUFFER_SIZE];
+    char buf2[BUFFER_SIZE];
+
+    scanf("%s", fileName); //파일의 이름을 입력받는다
+
+    FILE *fp = fopen(fileName, "r"); //파일에 접근하기 위해서 파일을 open
+    if (fp == NULL)
+    {
+        printf("Open failed.\n");
+        return;
+    }
+    while ((fscanf(fp, "%s", buf1) != EOF))  //파일의 끝에 도달할 때 까지 반복해서 이름과 전화번호를 읽어서 배열에 저장한다
+    { 
+        fscanf(fp, "%s", buf2);
+        names[n] = strdup(buf1);
+        numbers[n] = strdup(buf2);
+        n++;
+    }
+    fclose(fp); //파일을 닫아준다
+}
+
+void save(){
+    int i;
+    char fileName[BUFFER_SIZE];
+    char tmp[BUFFER_SIZE];
+
+    scanf("%s",tmp);
+    scanf("%s", fileName);
+
+    FILE *fp = fopen(fileName, "w");
+    if(fp == NULL){
+        printf("Open failed.\n");
+        return;
+    }
+    for(i=0;i<n;i++){
+        fprintf(fp,"%s %s\n", names[i],numbers[i]);
+    }
+
+    fclose(fp);
 }
