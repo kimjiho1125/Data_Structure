a32744753a3ee1cc88d374017c7b5ea2661d850a
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2022-01-26 16:47:14 +0900

    phonebook03

--- phonebook03.c
+++ phonebook03.c
@@ -12,6 +12,18 @@ int capacity = INIT_CAPACITY; /* size of arrays*/
 int n = 0;                    /* number of people in phone directory */
 
 /* function prototypes here */
+void init_directory();
+void process_command();
+void load(char *fileName);
+void add(char *name,char *number);
+void status();
+void find(char *name);
+int search(char *name);
+void remove(char* name);
+void save(char *fileName);
+void reallocate();
+
+
 char delim[] = " ";
 int main()
 {
@@ -40,4 +52,178 @@ int read_line(char str[], int limit)
     str[i] = '\0'; /* 마지막에 null character를 추가해준다. */
 
     return i; /* 실제로 읽은 문자수를 반환한다. */
+}
+
+void process_command(){
+    char command_line[BUFFER_SIZE]; /* 한 라인을 통채로 읽어오기 위한 버퍼*/
+    char *command, *argument1, *argument2;
+
+    while(1){
+        printf("$ ");
+
+        if(read_line(command_line,BUFFER_SIZE)<=0) /* 명령줄을 통채로 읽는다 */
+            continue;                              /* 아무것도 입력안하면 continue(while문의 시작으로 돌아감) */
+        
+        command = strtok(command_line,delim);  /* 첫 번째 토큰은 명령어이다 */
+        if(command == NULL) continue;
+        
+        if(strcmp(command, "read") == 0){
+            argument1 = strtok(NULL,delim); /* read명령에서 두번째 토큰은 파일명이다 */
+            if(argument1 == NULL){
+                printf("File name required.\n");
+                continue;
+            }
+            load(argument1); /* 파일명을 인자로 주면서 load를 호출한다 */
+        }
+        else if(strcmp(command, "add") == 0){
+            argument1 = strtok(NULL,delim);
+            argument2 = strtok(NULL,delim);
+
+            if(argument1 == NULL || argument2 == NULL){
+                printf("Invalid arguments.\n");
+                continue;
+            }
+            add(argument1,argument2);
+            printf("%s was added successfully.\n", argument1);
+        }
+        else if(strcmp(command,"find") == 0){
+            argument1 = strtok(NULL, delim);
+            if(argument1 == NULL){
+                printf("Invalid arguments.\n");
+                continue;
+            }
+            find(argument1);
+        }
+        else if(strcmp(command, "status") == 0) 
+            status();
+        else if(strcmp(command,"delete")==0){
+            argument1 = strtok(NULL,delim);
+            if(argument1 == NULL){
+                printf("Invalid arguments.\n");
+                continue;
+            }
+            remove(argument1);
+        }
+        else if(strcmp(command,"save")==0){
+            argument1 = strtok(NULL,delim);
+            argument2 = strtok(NULL,delim);
+
+            if(argument1 == NULL || strcmp("as",argument1) != 0 || argument2 == NULL){
+                printf("Invalid command format.\n");
+                continue;
+            }
+            save(argument2);
+        }
+        else if(strcmp(command,"exit") == 0)
+            break;
+    }
+}
+
+void load(char *fileName){
+    char buf1[BUFFER_SIZE];
+    char buf2[BUFFER_SIZE];
+
+    FILE *fp = fopen(fileName,"r");
+    if(fp==NULL){
+        printf("Open failed.\n");
+        return;
+    }
+    while((fscanf(fp,"s",buf1)!=EOF)){
+        fscanf(fp,"%s",buf2);
+        add(buf1,buf2);
+    }
+    fclose(fp);
+}
+
+void add(char *name,char *number){
+    if(n>=capacity)
+        reallocate();
+    int i=n-1;
+    while(i>=0 && strcmp(names[i],name)>0){
+        names[i+1] = names[i];
+        numbers[i+1] = numbers[i];
+        i--;
+    }
+    names[i+1] = strdup(name);
+    numbers[i+1] = strdup(number);
+
+    n++;
+}
+
+void status(){
+    int i;
+    for(i=0;i<n;i++){
+        printf("%s %s\n", names[i],numbers[i]);
+    }
+    printf("Total %d persons.\n", n);
+}
+
+void find(char *name){
+    int index = search(name);
+    if(index == -1)
+        printf("No person named '%s' exist.\n", name);
+    else 
+        printf("%s\n", numbers[index]);
+}
+
+int search(char *name){
+    int i;
+    for(i=0;i<n;i++){
+        if(strcmp(name,names[i])==0){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void remove(char *name){
+    
+    int i = search(name);
+    if( i == -1){
+        printf("No person named '%s' exist", name);
+        return;
+    }
+
+    int j = i;
+    for(; j<n-1; j++){
+        names[j] = names[j+1];
+        numbers[j] = numbers[j+1]; 
+    }
+
+    n--;
+    printf("'%s' was deleted successfully.\n", name);
+}
+
+void save(char *fileName){
+    int i;
+    FILE *fp = fopen(fileName,"w");
+    if(fp==NULL){
+        printf("Open failed.\n");
+        return;
+    }
+
+    for(i=0;i<n;i++){
+        fprintf(fp, "%s %s\n",names[i],numbers[i]);
+    }
+    fclose(fp);
+}
+
+void reallocate(){
+    
+    int i;
+
+    capacity *= 2;
+    char **tmp1 = (char **)malloc(capacity*sizeof(char *));
+    char **tmp2 = (char **)malloc(capacity*sizeof(char *));
+
+    for (i=0;i<n;i++){
+        tmp1[i] = names[i];
+        tmp2[i] = numbers[i];
+    }
+
+    free(names);
+    free(numbers);
+
+    names = tmp1;
+    numbers = tmp2;
 }
